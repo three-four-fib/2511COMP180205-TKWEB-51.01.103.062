7abf9fc825b13c77101eaa4af5e44f6f63e7a296
three-four-fib <threefourfivefourfivesix@gmail.com>
2025-10-25 12:25:46 +0700

    Deploy built React site for De_tai_cuoi_khoa

--- website/vite.config.js
+++ website/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/2511COMP180205-TKWEB-51.01.103.062/", 
+  base: '/2511COMP180205-TKWEB-51.01.103.062/website/',
 });
 
 
